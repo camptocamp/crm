9699fa42621a4b033e9cea828ad829f7dc4bab02
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-06-08 18:37:32 +0200

    Create module crm_lead_partner_role

---
+++ crm_lead_partner_role/models/crm_role.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models, fields
+
+
+class CrmRole(models.Model):
+
+    _name = 'crm.role'
+
+    name = fields.Char(required=True)
