ccea4770191c15bd885f7ab41ebc2e082ebca7d7
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-06-11 14:35:40 +0200

    fixup! Create module crm_lead_project

--- crm_lead_project/wizard/crm_lead_to_opportunity.py
+++ crm_lead_project/wizard/crm_lead_to_opportunity.py
@@ -12,7 +12,7 @@ class Lead2OpportunityPartner(models.TransientModel):
     name = fields.Selection(selection_add=[
         ('convert_create_project', 'Convert to opportunity and create project')
     ])
-    use_tasks = fields.Boolean()
+    use_tasks = fields.Boolean(default=True)
 
     @api.multi
     def _prepare_project_values(self, lead):
@@ -28,10 +28,9 @@ class Lead2OpportunityPartner(models.TransientModel):
     def action_apply(self):
         self.ensure_one()
 
-        import pdb; pdb.set_trace()
         if self.name != 'convert_create_project':
             return super(Lead2OpportunityPartner, self).action_apply()
-        # TODO Check if needed
+        # TODO Check if really needed
         if len(self.env.context.get('active_ids')) > 1:
             raise UserError(_('Cannot create project with two leads.'))
         # Prepare opportunity conversion as it's done by odoo
@@ -42,10 +41,6 @@ class Lead2OpportunityPartner(models.TransientModel):
             values['partner_id'] = self.partner_id.id
         leads = self.env['crm.lead'].browse(
             self._context.get('active_ids', []))
-
-        # TODO Check if needed
-        if len(leads) > 1:
-            raise UserError(_('Cannot create project with more than one lead.'))
         # Convert to opportunity as it's done by odoo
         values.update({'lead_ids': leads.ids, 'user_ids': [self.user_id.id]})
         self._convert_opportunity(values)
